eb6dc4b5aff6eb11a2acef1607cec73e9ea81046
kumarstack55 <kumarstack55@gmail.com>
2020-09-27 12:00:53 +0000

    :book: Add .readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # The master toctree document.
 # Add master_doc to avoid "Sphinx error: missing master file" on
